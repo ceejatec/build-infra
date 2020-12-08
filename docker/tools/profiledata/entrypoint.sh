0eb751ec2a2a9b586664306cf65ac8789b32a2e4
Blair Watt <blair.watt@couchbase.com>
2020-12-08 16:37:36 +0000

    Windows image changes
    - Create merge modules dir in win2012 ansible playbook
    - Remove clearcompressionflag from 2012 kb installs
    - Tidy packer builds, fix 2012 sysprep
    - Force stop analytics jenkins

--- docker/tools/profiledata/entrypoint.sh
+++ docker/tools/profiledata/entrypoint.sh
@@ -32,14 +32,14 @@ if [ "$AWS_EXECUTION_ENV" != "" ]; then # We're in AWS
     mkdir -p "/home/couchbase/couchbase-server/$subdir"
   done
 
-  echo "$couchbase_server_macos_cv_ssh_config"     >/home/couchbase/couchbase-server/cv/macos/.ssh/config
-  echo "$couchbase_server_shared_ssh_environment"  >/home/couchbase/couchbase-server/shared/.ssh/environment
   echo "$couchbase_server_shared_gitconfig"        >/home/couchbase/couchbase-server/shared/.gitconfig
+  echo "$couchbase_server_shared_ssh_environment"  >/home/couchbase/couchbase-server/shared/.ssh/environment
   echo "$couchbase_server_linux_cv_ssh_config"     >/home/couchbase/couchbase-server/cv/linux/.ssh/config
+  echo "$couchbase_server_macos_cv_ssh_config"     >/home/couchbase/couchbase-server/cv/macos/.ssh/config
   echo "$couchbase_server_linux_build_ssh_config"  >/home/couchbase/couchbase-server/build/linux/.ssh/config
   echo "$couchbase_server_windows_config"          >/home/couchbase/couchbase-server/build/windows/.ssh/config
-  echo "$couchbase_server_windows_authorized_keys" >/home/couchbase/couchbase-server/build/windows/.ssh/authorized_keys
   echo "$couchbase_server_windows_environment"     >/home/couchbase/couchbase-server/build/windows/.ssh/environment
+  echo "$couchbase_server_windows_authorized_keys" >/home/couchbase/couchbase-server/build/windows/.ssh/authorized_keys
 fi
 
 exec $@
