3e38ecbd41a8e5c6fdde4dc57a48b99810caa570
Blair Watt <blair.watt@couchbase.com>
2020-12-16 16:34:07 +0000

    Fix CPU value

--- terraform/aws-jenkins/services/jenkins/workers.tf
+++ terraform/aws-jenkins/services/jenkins/workers.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_task_definition" "worker" {
   execution_role_arn = var.ecs_role.arn
   # requires_compatibilities = ["FARGATE"]
 
-  cpu    = each.key == "ansible" ? 1 : var.worker_cpu
+  cpu    = each.key == "ansible" ? 1024 : var.worker_cpu
   memory = each.key == "ansible" ? 2048 : var.worker_memory
 
   network_mode = "awsvpc"
