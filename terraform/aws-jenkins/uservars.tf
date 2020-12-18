2933c8cf2b3b86bc7c76498c90c41735426bdb20
Ming Ho <ming.ho@couchbase.com>
2020-12-18 15:15:40 -0800

    more automation for proget:   automated deregistration and registration   move docker-network creation into autoscaling.
    -Ming Ho

--- terraform/aws-jenkins/uservars.tf
+++ terraform/aws-jenkins/uservars.tf
@@ -52,7 +52,7 @@ locals {
   # proget
   proget_name            = "proget"
   proget_subdomain       = "build"
-  proget_ami             = "ami-0ebf1f6ace7f1f2e4"
+  proget_ami             = "ami-01accb82117ea785e"
   proget_instance_type   = "c5a.large"
 
   # downloads
